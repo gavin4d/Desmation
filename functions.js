207f521560ef4e34e0be905781fa53f362b29639
gavin4d <gavin4d05@gmail.com>
2021-09-16 14:56:48 -0700

    added download

--- functions.js
+++ functions.js
@@ -1,126 +1,153 @@
+var zip = JSZip();
 
+var elt = document.getElementById('calculator');
+var calculator = Desmos.GraphingCalculator(elt,{
+    expressions : true,
+    border : true,
+    zoomButtons : true,
+    pasteGraphLink : true});
 
-        var elt = document.getElementById('calculator');
-        var calculator = Desmos.GraphingCalculator(elt,{
-            expressions : true,
-            border : true,
-            zoomButtons : true,
-            pasteGraphLink : true});
+var frameNumber = 0;
 
-        var frameNumber = 0;
+var btnElt = document.getElementById('screenshot-button');
+btnElt.addEventListener('click', captureScreenshot);
 
-        var btnElt = document.getElementById('screenshot-button');
-        btnElt.addEventListener('click', captureScreenshot);
-        
-        var btnElt2 = document.getElementById('view-button');
-        btnElt2.addEventListener('click', loadFrame);
+var btnElt2 = document.getElementById('view-button');
+btnElt2.addEventListener('click', loadFrame);
 
-        var btnElt3 = document.getElementById('save-button');
-        btnElt3.addEventListener('click', saveFrame);
+var btnElt3 = document.getElementById('save-button');
+btnElt3.addEventListener('click', saveFrame);
 
-        var btnElt4 = document.getElementById('record-button');
-        btnElt4.addEventListener('click', record);
-            
-        calculator.setExpression({ id: '1', latex: 'y=x^2' });
+var btnElt4 = document.getElementById('record-button');
+btnElt4.addEventListener('click', record);
 
-        function setImageSrc(data) {
+var btnElt5 = document.getElementById('download-button');
+btnElt5.addEventListener('click', function () {
+    zip.generateAsync({type:"blob"}).then(function (blob) { // 1) generate the zip file
+        download(URL.createObjectURL(blob), "hello.zip");   // 2) trigger the download
+    }, function (err) {
+        btnElt5.text(err);
+    })
+}
+);
 
-            var img = document.getElementById('image');
-            img.innerHTML = data;
+const download = (path, filename) => {
+    // Create a new link
+    const anchor = document.createElement('a');
+    anchor.href = path;
+    anchor.download = filename;
 
-        }
+    // Append to the DOM
+    document.body.appendChild(anchor);
 
-        function captureScreenshot () {
+    // Trigger `click` event
+    anchor.click();
 
-        calculator.updateSettings({
-            showXAxis : false,
-            showYAxis : false,
-            showGrid : false
-        });
+    // Remove element from DOM
+    document.body.removeChild(anchor);
+}; 
+    
+calculator.setExpression({ id: '1', latex: 'y=ax^2' });
 
-        calculator.asyncScreenshot({
-            mode: 'stretch',
-            format: 'svg',
-            width: 800,
-            height: 800,
-            mathBounds: { left: -5, right: 5, top: 5, bottom:-5 }
-        }, setImageSrc);
+function setImageSrc(data) {
 
-        calculator.updateSettings({
-            showXAxis : true,
-            showYAxis : true,
-            showGrid : true
-        });
-
-        }
-
-        function saveFrame(name) {
-        //console.log(calculator.getExpressions());
-        calculator.setExpression({
-            id: '2',
-            color: '#bbbbbb'
-        });
-
-        var data = document.getElementById("image").innerHTML;
-        //console.log(data);
-        sessionStorage.setItem("img", data);
-
-        }
-
-        function loadFrame() {
-
-        var data = sessionStorage.getItem("img");
-        //console.log(data);
         var img = document.getElementById('image');
         img.innerHTML = data;
 
-        }
+}
 
-        function record() {
+function captureScreenshot () {
 
-            calculator.setExpression({
-                id: '2',
-                latex: 'a=' + frameNumber
-            });
-            
-            updateViewSettings(false)
+    calculator.updateSettings({
+        showXAxis : false,
+        showYAxis : false,
+        showGrid : false
+    });
 
-            calculator.asyncScreenshot({
-                mode: 'stretch',
-                format: 'svg',
-                width: 800,
-                height: 800,
-                mathBounds: { left: -5, right: 5, top: 5, bottom:-5 }
-            }, recordRepeat);
+    calculator.asyncScreenshot({
+        mode: 'stretch',
+        format: 'svg',
+        width: 800,
+        height: 800,
+        mathBounds: { left: -5, right: 5, top: 5, bottom:-5 }
+    }, setImageSrc);
 
-            updateViewSettings(true)
+    calculator.updateSettings({
+        showXAxis : true,
+        showYAxis : true,
+        showGrid : true
+    });
 
-        }
+}
 
-        function recordRepeat(data) {
+function saveFrame(name) {
+    //console.log(calculator.getExpressions());
+    calculator.setExpression({
+        id: '2',
+        color: '#bbbbbb'
+    });
 
-        sessionStorage.setItem('Frame_'+ ('0000' + frameNumber).slice(-4), data);
+    var data = document.getElementById("image").innerHTML;
+    //console.log(data);
+    sessionStorage.setItem("img", data);
 
-        document.getElementById("image").innerHTML = data;
-        
-        frameNumber ++;
+}
 
-        if (frameNumber < 10) {
+function loadFrame() {
 
-            record()
+    var data = sessionStorage.getItem("img");
+    //console.log(data);
+    var img = document.getElementById('image');
+    img.innerHTML = data;
 
-        } else {
-            frameNumber = 0
-        }
+}
 
-        }
+function record() {
 
-        function updateViewSettings(boolean) {
+    calculator.setExpression({
+        id: '2',
+        latex: 'a=' + frameNumber
+    });
 
-        calculator.updateSettings({
-            showXAxis : boolean,
-            showYAxis : boolean,
-            showGrid : boolean
-        });
+    updateViewSettings(false)
 
-        }
+    calculator.asyncScreenshot({
+        mode: 'stretch',
+        format: 'svg',
+        width: 800,
+        height: 800,
+        mathBounds: { left: -5, right: 5, top: 5, bottom:-5 }
+    }, recordRepeat);
+
+    updateViewSettings(true)
+
+}
+
+function recordRepeat(data) {
+
+    //sessionStorage.setItem('Frame_'+ ('0000' + frameNumber).slice(-4), data);
+    zip.file('Frame_'+ ('0000' + frameNumber).slice(-4) + '.svg', data);
+
+    document.getElementById("image").innerHTML = data;
+    
+    frameNumber ++;
+
+    if (frameNumber < 10) {
+
+        record()
+
+    } else {
+        frameNumber = 0
+    }
+
+}
+
+function updateViewSettings(boolean) {
+
+    calculator.updateSettings({
+        showXAxis : boolean,
+        showYAxis : boolean,
+        showGrid : boolean
+    });
+
+}
